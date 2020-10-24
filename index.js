ea057558020092955d7228e7b4d514f2c3db79a9
oatbuu <60160283@go.buu.ac.th>
2020-10-24 16:09:22 +0700

    nothing to edit

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-    return res.send('Hello')
-})
+    return res.send('Hello!')
+})  
 
 app.listen(port, () => {
     console.log(`Start Express application at ${port}`)
--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,22 @@
 const assert = require('assert')
+const request =require('supertest')
+const app =require('../index')
+const app2 =require('../item')
 
 describe('Validate setting the test', () => {
     it('should be ok', () => {
-        assert.ok(false)
+        assert.ok(true)
     })
+})
+
+describe('GET "/"', () => {
+    it('respond with Hello!'), (done) => {
+        request(app).get('/').expect('Hello!',done)
+    }
+})
+
+describe('GET "/item"', () => {
+    it('respond with Hello!'), (done) => {
+        request(app2).get('/').expect('Hello!',done)
+    }
 })
